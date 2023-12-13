493bb607675a4fb63bb069dbf4e2f2268db17b04
alinadubchak <alina.dubchak.ev@gmail.com>
2023-12-13 20:38:15 +0200

    added user model

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const expHbs = require('express-handlebars')
-const Handlebars = require('handlebars')
-const path = require('path')
-const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const expHbs = require('express-handlebars');
+const Handlebars = require('handlebars');
+const path = require('path');
+const mongoose = require('mongoose');
+const mongoUrl =
+  'mongodb+srv://yarikkot04:tTRT5ZfVYfTh2Ugo@cluster0.8iwqsa4.mongodb.net/shop';
+const {
+  allowInsecurePrototypeAccess,
+} = require('@handlebars/allow-prototype-access');
+const app = express();
 
-const mainRoute = require('./routes/main')
+const mainRoute = require('./routes/main');
 
 const hbs = expHbs.create({
   handlebars: allowInsecurePrototypeAccess(Handlebars),
   defaultLayout: 'main',
-  extname: 'hbs'
-})
+  extname: 'hbs',
+});
 
-app.engine('hbs', hbs.engine)
-app.set('view engine', 'hbs')
-app.set('views', 'views')
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
 
-app.use(bodyParser.json())
-app.use(express.static(path.resolve(__dirname, './public')))
-app.use(express.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.urlencoded({ extended: true }));
 
-
-app.use('/', mainRoute)
+app.use('/', mainRoute);
 
 async function main() {
-  const PORT = process.env.PORT || 3000
+  const PORT = process.env.PORT || 3000;
+  await mongoose.connect(mongoUrl);
   app.listen(PORT, () => {
-    console.log(`Server is running on port:${PORT}`)
-  })
+    console.log(`Server is running on port:${PORT}`);
+  });
 }
-main()
-
-
+main();
