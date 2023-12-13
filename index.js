8ad1285c7851fa064a498f7b47d2f2fb078f859d
yarikkot04 <yarikkotenkoim13@gmail.com>
2023-12-13 20:20:54 +0200

    Added structure to the project.
    Added handlebar.

--- index.js
+++ index.js
@@ -1,116 +1,36 @@
-let register = $('#link_sign_up');
-let reset = $('#test_reset');
-let link_reg = $('#link_reg');
-let btn = $('#btn');
-let text = $('#text');
+const express = require('express')
+const bodyParser = require('body-parser')
+const expHbs = require('express-handlebars')
+const Handlebars = require('handlebars')
+const path = require('path')
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access')
+const app = express()
 
-function toggleInputType() {
-  let passwordInput = $('#passwordInput');
-  let eyeIcon = $('#eye-icon');
+const mainRoute = require('./routes/main')
 
-  if (passwordInput.attr('type') === 'password') {
-    passwordInput.attr('type', 'text');
-    eyeIcon.attr('src', './icons/eye.svg');
-  } else {
-    passwordInput.attr('type', 'password');
-    eyeIcon.attr('src', './icons/eye_2.svg');
-  }
+const hbs = expHbs.create({
+  handlebars: allowInsecurePrototypeAccess(Handlebars),
+  defaultLayout: 'main',
+  extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(bodyParser.json())
+app.use(express.static(path.resolve(__dirname, './public')))
+app.use(express.urlencoded({ extended: true }))
+
+
+app.use('/', mainRoute)
+
+async function main() {
+  const PORT = process.env.PORT || 3000
+  app.listen(PORT, () => {
+    console.log(`Server is running on port:${PORT}`)
+  })
 }
+main()
 
-function login() {
-  let username = $('#usernameInput').val();
-  let password = $('#passwordInput').val();
 
-  let formData = {
-    username: username,
-    password: password,
-  };
-
-  $.ajax({
-    // fetch('http://localhost:3000/login'
-    type: 'POST',
-    url: 'http://localhost:3000/login',
-    contentType: 'application/json',
-    data: JSON.stringify(formData),
-    success: function (data) {
-      console.log('Success:', data, formData);
-    },
-    error: function (error) {
-      console.error('Error:', error);
-    },
-  });
-}
-
-function registerFetch() {
-  let username = $('#usernameInput').val();
-  let password = $('#passwordInput').val();
-  let mail = $('#mailInput').val();
-
-  let status;
-  if ($('#inlineCheckbox1').is(':checked')) {
-    status = 'Worker';
-  } else if ($('#inlineCheckbox2').is(':checked')) {
-    status = 'User';
-  } else {
-    status = 'Undefined';
-  }
-
-  let formData = {
-    username: username,
-    password: password,
-    mail: mail,
-    status: status,
-  };
-
-  $.ajax({
-    // fetch('http://localhost:3000/register'
-    type: 'POST',
-    url: 'http://localhost:3000/register',
-    contentType: 'application/json',
-    data: JSON.stringify(formData),
-    success: function (data) {
-      console.log('Success:', data, formData);
-    },
-    error: function (error) {
-      console.error('Error:', error);
-    },
-  });
-}
-
-function addSignUpField() {
-  const register = $('<div>').html(`
-                         <div class="input-group flex-nowrap">
-                    <span class="input-group-text" id="addon-wrapping">
-                      <img src="./icons/mail.svg" alt="" />
-                    </span>
-                    <input 
-                    type='email'
-                    id="mailInput"
-                    class='form-control' 
-                    placeholder='Email'
-                    aria-label='Password' 
-                    aria-describedby='addon-wrapping'
-                     />
-                  </div>
-                  <div id="checkBoxs">
-                    <input type="checkbox" class='form-check-input' id='inlineCheckbox1' value='option1'/>
-                    <label class="form-check-label"
-                        for="inlineCheckbox1" id="worker">Worker</label>
-                    <input type="checkbox" class='form-check-input'
-                      id='inlineCheckbox2' value='option2' />
-                    <label class="form-check-label"
-                        for="inlineCheckbox2">User</label>
-                  </div>
-    `);
-
-  $('#SignUpFieldsContainer').append(register);
-}
-
-register.on('click', function () {
-  link_reg.remove();
-  reset.remove();
-  btn.html('Sign Up');
-  btn.off('click');
-  btn.on('click', registerFetch);
-  text.html('Get Started Now!');
-});
